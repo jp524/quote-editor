5f04db42533c1c373f0fe7b6a729cd9a2249b7b2
JP <85654561+jp524@users.noreply.github.com>
2023-01-28 10:28:39 -0500

    Add LineItemDate model and associations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,8 @@
 class Quote < ApplicationRecord
-  validates :name, presence: true
-
+  has_many :line_item_dates, dependent: :destroy
   belongs_to :company
+
+  validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
 
